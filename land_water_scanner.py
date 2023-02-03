15f8b0184e4805c3963115afc5b42b0349caa674
PeachAuthor <alexiliarski@gmail.com>
2023-02-03 15:28:25 -0600

    changed to numpy

--- land_water_scanner.py
+++ land_water_scanner.py
@@ -2,22 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-# reads Land_Water_Access.csv and returns a list of lists
+# reads Land_Water_Access.csv and returns a numpy array of arrays
 def land_water_scanner():
-    # # open file
-    # file = open("Land_Water_Access.csv", "r")
-    # # read file
-    # file = file.read()
-    # # split file into lines
-    # file = file.split("\n")
-    # # split lines into lists
-    # for i in range(len(file)):
-    #     file[i] = file[i].split(",")
-    # # return list of lists
-    # return file
-
-    df = pd.read_csv("Land_Water_Access.csv")
-    return df
+    arr = np.loadtxt("Land_Water_Access.csv", delimiter=",", dtype=str)
+    return arr
 
 # if __name__ == "__main__":
 #     print(land_water_scanner())
