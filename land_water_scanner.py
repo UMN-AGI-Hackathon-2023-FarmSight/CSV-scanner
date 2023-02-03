25903e5771242640769a5ed5a7ee507dd9425f0e
PeachAuthor <alexiliarski@gmail.com>
2023-02-03 15:31:41 -0600

    changed to pandas

--- land_water_scanner.py
+++ land_water_scanner.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-# reads Land_Water_Access.csv and returns a numpy array of arrays
+# reads Land_Water_Access.csv and returns a pandas array of arrays
 def land_water_scanner():
-    arr = np.loadtxt("Land_Water_Access.csv", delimiter=",", dtype=str)
-    return arr
+    df = pd.read_csv("Land_Water_Access.csv")
+    return df
 
-# if __name__ == "__main__":
-#     print(land_water_scanner())
+if __name__ == "__main__":
+    print(land_water_scanner())
